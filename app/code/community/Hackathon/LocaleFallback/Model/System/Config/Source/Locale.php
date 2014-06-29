aa357a7eb3e5a78c98b2029a583a75da41ae7571
MaWoScha <manfredwormschaefer@gmail.com>
2014-06-29 16:26:19 +0200

    Datei-Kopfdaten nachgetragen

--- app/code/community/Hackathon/LocaleFallback/Model/System/Config/Source/Locale.php
+++ app/code/community/Hackathon/LocaleFallback/Model/System/Config/Source/Locale.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * @category Hackathon
+ * @package Hackathon_LocaleFallback
+ * @author Bastian Ike <b-ike@b-ike.de>
+ * @developer 
+ * @version 0.1.0
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)  
+ */
 class Hackathon_LocaleFallback_Model_System_Config_Source_Locale
 {
     public function toOptionArray()
